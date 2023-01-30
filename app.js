229fab4484cc0f0a0086f674e32fa56fd6901700
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-30 19:58:52 +0100

    Basic routes added

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ app.get("/", (req, res) => {
 	res.render("home", { homeStartingContent: homeStartingContent })
 })
 
+app.get("/about", (req, res) => {
+	res.render("about", { aboutContent: aboutContent })
+})
+
+app.get("/contact", (req, res) => {
+	res.render("contact", { contactContent: contactContent })
+})
+
+app.get("/compose", (req, res) => {
+	res.render("compose")
+})
+
 app.listen(3000, () => {
 	console.log("App is running on port 3000")
 })
